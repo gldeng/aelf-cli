5e8e870dfd0893aa234365265b7841d352c483e2
gldeng <realgldeng@gmail.com>
2024-11-28 15:27:10 +0800

    Log transfer information

--- AElf.Cli/Scripts/TransferScript.cs
+++ AElf.Cli/Scripts/TransferScript.cs
@@ -18,5 +18,14 @@ public class TransferScript : Script
     public override async Task RunAsync()
     {
         await this.TransferTokenAsync(Arguments.To, Arguments.Amount);
+        var record = TransactionHistory.GetLastTransaction();
+        if (record.Result.Status == TransactionResultStatus.Mined)
+        {
+            Console.WriteLine($"Transferred {Arguments.Amount} {Arguments.Symbol} at TxId {record.TransactionId} in block {record.Result.BlockNumber}.");
+        }
+        else
+        {
+            Console.WriteLine($"Transaction {record.TransactionId} failed in block {record.Result.BlockNumber}.");
+        }
     }
 }
